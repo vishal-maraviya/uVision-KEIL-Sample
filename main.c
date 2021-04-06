4d8c6e189b3dc6c552106423e04e90f39088c740
Vishal Maraviya <78132709+vishal-maraviya@users.noreply.github.com>
2021-04-06 11:29:03 +0530

    main.c updated
    Removed unnecessary code.

---
+++ main.c
@@ -0,0 +1,46 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V0.00
+ * @brief    Sample main.c file
+ *
+ * 
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+#define PLL_CLOCK       192000000
+
+void SYS_Init(void)
+{
+
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+
+    /* Enable HXT clock (external XTAL 12MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(PLL_CLOCK);
+
+    /* Set PCLK0/PCLK1 to HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+}
+
+int main(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Waiting for interrupts */
+	
+    while(1);
+}
